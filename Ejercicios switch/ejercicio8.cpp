5d4e70727afa0d82df2b9a360fa1174a8ad6d2c4
juanjo-guerra <00079926@clases.edu.sv>
2026-05-10 11:22:46 -0600

    :white_check_mark:Actualizar ejercicio 8

--- Ejercicios switch/ejercicio8.cpp
+++ Ejercicios switch/ejercicio8.cpp
@@ -3,28 +3,40 @@ using namespace std;
 
 int main(){
 
-    float a,b,c;
+    float a, b, c;
 
-    cout<<"---Bienvenido ingrese los lados para verificar que tipo de triangulo es---"<<endl;
-    cout<<"Ingrese el primer lado"<<endl;
-    cin>>a;
-
-    cout<<"Ingrese el segundo lado"<<endl;
-    cin>>b;
-
-    cout<<"Ingrese el tercer lado"<<endl;
-    cin>>c;
-
-
-    switch (a,b,c)
-    {
-    case:
-        /* code * /
-        break;
+    cout << "--- Bienvenido, ingrese los lados para verificar el tipo de triangulo ---" << endl;
     
-    default:
-        break;
+    cout << "Ingrese el primer lado: ";
+    cin >> a;
+
+    cout << "Ingrese el segundo lado: ";
+    cin >> b;
+
+    cout << "Ingrese el tercer lado: ";
+    cin >> c;
+
+    
+    if (a <= 0 || b <= 0 || c <= 0 || 
+        (a + b <= c) || (a + c <= b) || (b + c <= a)) {
+        cout << "Los lados ingresados NO forman un triangulo valido." << endl;
+        return 0;
     }
 
+    
+    int iguales = (a == b) + (b == c) + (a == c);
 
+    switch (iguales){
+        case 3:
+            cout << "Es un triangulo EQUILATERO (los 3 lados son iguales)" << endl;
+            break;
+        case 1:
+            cout << "Es un triangulo ISOSCELES (2 lados son iguales)" << endl;
+            break;
+        case 0:
+            cout << "Es un triangulo ESCALENO (todos los lados son diferentes)" << endl;
+            break;
+    }
+
+    return 0;
 }
