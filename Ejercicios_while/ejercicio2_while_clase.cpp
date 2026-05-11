669b3d57fc5e643562f42c9d57eac912a9042ce5
juanjo-guerra <00079926@clases.edu.sv>
2026-05-11 12:36:52 -0600

    :memo: nueva carpeta

---
+++ Ejercicios_while/ejercicio2_while_clase.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int n, suma;
+
+
+    
+    cout<<"Ingresa tu numero";
+    while (n0)
+    {
+        
+        
+        cin>>n;
+        
+    }
+    suma= n + suma;
+    
+    
+
+}
